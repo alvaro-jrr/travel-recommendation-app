bfc1f97a07cd8b8f2bb29197f74586ffe0c1fe2e
Alvaro Resplandor <alvarojrr79@gmail.com>
2025-01-28 23:46:34 -0400

    chore: setup prettier

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
+// Supports weights 100-900
+import "@fontsource-variable/dm-sans";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+
+import { HomePage } from "./modules/home/pages/home-page";
+
 import "./index.css";
-import { HomePage } from "./features/home/home_page";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HomePage />
-  </StrictMode>
+  </StrictMode>,
 );
